c5a92cde719f00d388ab8aa67958c598cb2b9d0c
Tim <timmcmackenjr@gmail.com>
2021-07-06 12:59:01 -0500

    inital commit

--- 0x0E-Cookies_local_storage/README.md
+++ 0x0E-Cookies_local_storage/README.md
@@ -11,11 +11,12 @@ Read or watch:
 ## Learning Objectives:bulb:
 What you should learn from this project:
 
-
-*           mandatory
-*         
-
----
+* Understand how to create cookies using Javascript
+* Learn how to set specific settings for the cookie
+* Learn how to read cookies with Javascript
+* How to use js-cookie for easy cookie manipulation
+* How to use the browser web storage
+* The differences between local storage and session storage
 ---
 
 ## Author
--- 0x0E-Cookies_local_storage/src/1-index.js
+++ 0x0E-Cookies_local_storage/src/1-index.js
@@ -2,13 +2,19 @@
 const setCookies = () => {
     const firstname = document.getElementById("firstname").value;
     const email = document.getElementById("email").value;
+    let date = new Date();
+    date.setDate(date.getDate() + 10);
     document.cookie = `firstname=${firstname};`
     document.cookie = `email = ${email};`
+    document.cookie = `expires = ${date.toUTCString()};`
+    document.cookie = `path = /`;
 }
 
 const showCookies = () => {
     const cookie = document.cookie;
     const p = document.createElement("p");
-    p.innerHTML = `Cookies: ${cookie}`;
+    const email = getCookie("email");
+    const firstname = getCookie("firstname");
+    p.innerHTML = `Email: ${email} - Firstname: ${firstname}`;
     document.body.append(p);
 }
